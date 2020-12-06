9cd2e64e16767459514f07188ee32f23b8302584
Universum <programming@universegame.de>
2020-12-07 00:18:10 +0100

    finished simple summary, inspired by skate702.de

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website
+
+icons: https://iconmonstr.com
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var htmlBody = htmld.getElementsByTagName("body")[0];
+var htmlBody;
 
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  htmlBody.classList.add("darkmode");
-  htmlBody.classList.remove("lightmode");
-} else {
-  htmlBody.classList.add("lightmode");
-  htmlBody.classList.remove("darkmode");
+function setMode() {
+  if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    htmlBody.classList.add("darkmode");
+    htmlBody.classList.remove("lightmode");
+  } else {
+    htmlBody.classList.add("lightmode");
+    htmlBody.classList.remove("darkmode");
+  }
 }
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  //const newColorScheme = e.matches ? "dark" : "light";
+  setMode();
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  htmlBody = document.getElementById("body")
+  setMode();
+});
