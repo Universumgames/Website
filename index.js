db50d1e395e1e8473c5eb8a57c860befb4a63fd4
Universum <programming@universegame.de>
2020-12-07 21:42:51 +0100

    updated css hover, added colorful gradients

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var htmlBody;
+var connectives;
 
 function setMode() {
   if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
     htmlBody.classList.add("darkmode");
     htmlBody.classList.remove("lightmode");
+    Array.from(connectives).forEach(element => {
+      element.classList.add("darkmode");
+      element.classList.remove("lightmode");
+    });
   } else {
     htmlBody.classList.add("lightmode");
     htmlBody.classList.remove("darkmode");
+    Array.from(connectives).forEach(element => {
+      element.classList.add("lightmode");
+      element.classList.remove("darkmode");
+    });
   }
 }
 
@@ -17,5 +26,6 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e =
 
 window.addEventListener('DOMContentLoaded', (event) => {
   htmlBody = document.getElementById("body")
+  connectives = document.getElementsByClassName("connections");
   setMode();
 });
